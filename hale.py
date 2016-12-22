a3305c6c803c036c7dcceb6830de613384db47fe
Greg <gnfrazier@gmail.com>
2016-12-21 21:09:19 -0500

    Added docstrings and cleaned up unused code

--- hale.py
+++ hale.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 23 08:01:42 2016
+Scrape the menu from Hale and Hearty Soups website for fun on a cold winter day.
 
-
-
-<div class="select-wrapper"><!--
-  --><select name="js_dropdown" class="js-dropdown">
-
-     <option value="/menu/?location=17th-and-broadway"
-             selected="selected"
-             >17th &amp; Broadway</option>
-
-https://www.haleandhearty.com/menu/?location=
-
-@author: gfrazier
+Outputs the day's menu by location with prices.
 """
 
 import requests
@@ -26,6 +15,7 @@ import os
 import string
 
 def get_soup(url):
+    """Check for a 200 response from the website and return parsed soup object."""
 
     html = requests.get(url)
 
@@ -39,11 +29,15 @@ def get_soup(url):
         return none
 
 def extract_soup(html):
-        soup = BeautifulSoup(html.text, "html.parser")
-        return soup
+    """Extract the soup object from the passed HTML."""
+
+    soup = BeautifulSoup(html.text, "html.parser")
+    return soup
 
 def get_locations(soup):
+    """Return a list of store location URL parameters."""
 
+    #extract option tag with value="matching string"
     locs = soup.select('option[value^="/menu/?location="]')
     n = len(locs)
     #value iterable from BS4
@@ -52,33 +46,38 @@ def get_locations(soup):
     return locations
 
 def get_names(soup):
-    #extract option tag with value="matching string"
+    """Return a list of store location public names"""
+
     storeid = soup.find(id="our-menu").find_all('option')
     n = len(storeid)
     names = [storeid[i].text for i in range(n)]
     return names
 
 def get_menu_items(soup):
+    """Return a dictionary of key menu-items and value prices"""
 
-    #menu = soup.select("menu-item__name")
     items = soup.find_all(class_="menu-item__name") #has tags
     prices = soup.find_all(class_="menu-item__price") #has tags
     n = len(items)
 
     item_name = [items[i].text for i in range(n)]
     item_price = [prices[i].text for i in range(n)]
+    strname = [item_name[i].strip() for i in range(n)] #strip newlines
     strprice = [item_price[i].strip() for i in range(n)] #strip newlines
     justprice = [strprice[i].strip('\n\n+') for i in range(n)] #remove extra /n bits
-    menu_items = dict(zip(item_name, justprice)) #zip into dict with item:price
+    menu_items = dict(zip(strname, justprice)) #zip into dict with item:price
     return menu_items
 
 def form_url(site, loc):
+    """Concatenate site URI with location specific parameters"""
 
     url = site + loc
 
     return url
 
 def write_menu(date, sname, name, menu, path, file):
+    """Append data to output file passed as path, file"""
+
     file_name = path + file
     with open(file_name, "a") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
@@ -92,12 +91,12 @@ def main():
     date = arrow.now('US/Eastern').format('YYYY-MM-DD')
     site = 'https://www.haleandhearty.com'
     soup = get_soup(site)
-    #soup = extract_soup(html)
     locations = get_locations(soup)
     names = get_names(soup)
+
     for i in range(len(locations)):
-        # loc = (value['value']) #value iterable from BS4
-        #loc= ['17th-and-broadway'] #single loc for testing
+        """Iterate through locations list, append menu to output file."""
+        
         loc = locations[i]
         url = form_url(site, loc) #list value for testing
         soup = get_soup(url)
@@ -107,5 +106,8 @@ def main():
         write_menu(date, sname, name, menu, path, 'day-menu.csv')
         mcount = len(menu)
         print('Store {} has a menu of {} items.'.format(name, mcount))
+
+
 if __name__ == "__main__":
+
     main()
